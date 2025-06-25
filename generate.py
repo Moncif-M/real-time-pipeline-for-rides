49b009e51066207eaf4c7daf86a6bad57d0e8018
Viber <mouadmoncif1@gmail.com>
2025-06-26 00:16:12 +0100

    modified generation script

--- generate.py
+++ generate.py
@@ -1,5 +1,4 @@
-from datetime import datetime,timezone
-import time
+from datetime import datetime,timezone,timedelta
 import uuid
 import json 
 import random
@@ -10,13 +9,20 @@ def generate_location():
 producer = KafkaProducer(bootstrap_servers = 'localhost:9092',
                          value_serializer=lambda v : json.dumps(v).encode('utf-8'))
 
+
+def generate_random_timestamp():
+    now = datetime.now(timezone.utc)
+    delta = timedelta(days=random.uniform(0, 10))
+    return (now - delta).isoformat()
+
+
 while True :
     ride = {
         'ride_id' : str(uuid.uuid4()),
-        'timestamp' : datetime.now(timezone.utc).isoformat(),
+        'timestamp' : generate_random_timestamp(),
         'pickup_location': generate_location(),
         'dropoff_location' : generate_location(),
-        'passanger count' : random.randint(1,20),
+        'passanger_count' : random.randint(1,20),
         'price': round(random.uniform(20,100)),
         'driver_id': random.randint(1,4)
     }
